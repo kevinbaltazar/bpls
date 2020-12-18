5f134398f210568927fdbc33435e841b42c07cc4
ahgumasing <ahgumasing@gmail.com>
2020-12-18 18:37:45 +0800

    application

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/applynow', function () {
+    return view('applicant/applynow');
+});
+
