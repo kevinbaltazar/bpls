e14eea356de36b439cfbf5773e99f482fe3d3ab9
Jovert Palonpon <jovertical@gmail.com>
2021-01-03 12:12:22 +0800

    Hide admin nav link

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -17,12 +17,14 @@
                             Dashboard
                         </a>
 
-                        <a 
-                            href="{{ route('admin.admins.index') }}" 
-                            class="@if (Request::url() === route('admin.admins.index')) border-indigo-500 text-gray-900 @else border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 @endif inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                        >
-                            Admins
-                        </a>
+                        @role('superadmin')
+                            <a 
+                                href="{{ route('admin.admins.index') }}" 
+                                class="@if (Request::url() === route('admin.admins.index')) border-indigo-500 text-gray-900 @else border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 @endif inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                            >
+                                Admins
+                            </a>
+                        @endrole
                     </div>
                 </div>
 
