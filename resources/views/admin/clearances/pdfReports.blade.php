e1d61d57cb390fab0e2552935a1db12a97e2cea3
Ubuntu <jovertical@gmail.com>
2021-02-09 09:56:59 +0000

    Revert "gg"
    This reverts commit 35c13ed7d853738704f405f46157735c4ce67499.

--- resources/views/admin/clearances/pdfReports.blade.php
+++ resources/views/admin/clearances/pdfReports.blade.php
@@ -7,23 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    <table style="border: 1px">
-        <th>
-            cedula number
-        </th>
-        <th>
-            name
-        </th>
-        <th>
-            business name
-        </th>
-        @foreach ($reports as $report)
-        <tr>
-            <td>
-                {{$report->}}
-            </td>
-        </tr>
-        @endforeach
-    </table>
+
 </body>
 </html>
