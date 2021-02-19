dc2c29093a771580d72f56777cf3c87047144d0a
kevin <kevinngakasi27@gmail.com>
2021-02-19 08:11:37 +0800

    mock def

--- database/seeders/AdminsTableSeeder.php
+++ database/seeders/AdminsTableSeeder.php
@@ -17,59 +17,11 @@ class AdminsTableSeeder extends Seeder
     public function run()
     {
         $kevin = Admin::create([
-            'name' => 'Kevin Baltazar',
-            'email' => 'kevinngakasi27@gmail.com',
+            'name' => 'Abetteson Gumasing',
+            'email' => 'ahgumasin@gmail.com',
             'password' => Hash::make('password')
         ]);
 
         $kevin->assignRole('superadmin');
-
-        $abette = Admin::create([
-            'name' => 'Abette Gumasing',
-            'email' => 'ahgumasing@gmail.com',
-            'password' => Hash::make('password')
-        ]);
-
-        $abette->assignRole('superadmin');
-
-        $reviewer = Admin::create([
-            'name' => 'Candy Ignacio',
-            'email' => 'reviewer@bpls.site',
-            'password' => Hash::make('password')
-        ]);
-
-        $reviewer->assignRole('reviewer');
-
-        $inspector = Admin::create([
-            'name' => 'Darren Hermogenes',
-            'email' => 'inspector@bpls.site',
-            'password' => Hash::make('password')
-        ]);
-
-        $inspector->assignRole('inspector');
-
-        $approver = Admin::create([
-            'name' => 'Edgardo S. Feliciano',
-            'email' => 'approver@bpls.site',
-            'password' => Hash::make('password')
-        ]);
-
-        $approver->assignRole('approver');
-
-        $captain = Admin::create([
-            'name' => 'Raymond Castañeda',
-            'email' => 'captain@bpls.site',
-            'password' => Hash::make('password')
-        ]);
-
-        $captain->assignRole('approver');
-
-        $dispatcher = Admin::create([
-            'name' => 'Jessalyn Alberto',
-            'email' => 'dispatcher@bpls.site',
-            'password' => Hash::make('password')
-        ]);
-
-        $dispatcher->assignRole('dispatcher');
     }
 }
