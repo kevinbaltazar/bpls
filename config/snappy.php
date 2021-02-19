619990e6948f827b4366f2d8649e9431dd1e0255
kevin <kevinngakasi27@gmail.com>
2021-02-19 14:42:56 +0800

    snappy

--- config/snappy.php
+++ config/snappy.php
@@ -34,20 +34,20 @@ return [
     */
 
     'pdf' => [
-            'enabled' => true,
-            'binary'  => env('WKHTML_PDF_BINARY', '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"'),
-            'timeout' => false,
-            'options' => [],
-            'env'     => [],
-        ],
-        
-        'image' => [
-            'enabled' => true,
-            'binary'  => env('WKHTML_IMG_BINARY', '/usr/local/bin/wkhtmltoimage'),
-            'timeout' => false,
-            'options' => [],
-            'env'     => [],
-        ],
+        'enabled' => true,
+        'binary'  => env('WKHTML_PDF_BINARY', '/usr/local/bin/wkhtmltopdf'),
+        'timeout' => false,
+        'options' => [],
+        'env'     => [],
+    ],
+
+    'image' => [
+        'enabled' => true,
+        'binary'  => env('WKHTML_IMG_BINARY', '/usr/local/bin/wkhtmltoimage'),
+        'timeout' => false,
+        'options' => [],
+        'env'     => [],
+    ],
     
 ];
 
