68853f94683518d152c398dfdcb888f44df978fa
kevin <kevinngakasi27@gmail.com>
2021-02-27 12:10:00 +0800

    gg

--- database/seeders/AdminsTableSeeder.php
+++ database/seeders/AdminsTableSeeder.php
@@ -16,12 +16,52 @@ class AdminsTableSeeder extends Seeder
      */
     public function run()
     {
+        $kevin = Admin::create([
+            'name' => 'Kevin Baltazar',
+            'email' => 'kevinngakasi27@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $kevin->assignRole('superadmin');
+
         $abette = Admin::create([
-            'name' => 'Abetteson Gumasing',
+            'name' => 'Abette Gumasing',
             'email' => 'ahgumasing@gmail.com',
             'password' => Hash::make('password')
         ]);
 
         $abette->assignRole('superadmin');
+
+        $reviewer = Admin::create([
+            'name' => 'Juan Dela Cruz',
+            'email' => 'reviewer@bpls.site',
+            'password' => Hash::make('password')
+        ]);
+
+        $reviewer->assignRole('reviewer');
+
+        $inspector = Admin::create([
+            'name' => 'Iska',
+            'email' => 'inspector@bpls.site',
+            'password' => Hash::make('password')
+        ]);
+
+        $inspector->assignRole('inspector');
+
+        $approver = Admin::create([
+            'name' => 'Isko',
+            'email' => 'approver@bpls.site',
+            'password' => Hash::make('password')
+        ]);
+
+        $approver->assignRole('approver');
+
+        $dispatcher = Admin::create([
+            'name' => 'Juan',
+            'email' => 'dispatcher@bpls.site',
+            'password' => Hash::make('password')
+        ]);
+
+        $dispatcher->assignRole('dispatcher');
     }
 }
